1d489c73bc5c916c9734d8c75db9e83ce0230903
Jonathan Girard Viau <girardvjonathan@gmail.com>
2017-01-21 23:28:57 -0500

    CORS 3

--- app/index.js
+++ app/index.js
@@ -74,7 +74,7 @@ router.get(config.tendererChooseEndpoint, function(req, res, next) {
 });
 // Register API endpoints
 app.use('/api', router);
-app.use(function(req, res, next, next) {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
